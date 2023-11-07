e6cd6cdb8d0f84c9692c76473f4ef81fe0ad52a2
DiraApriliani <diraapriliani180@gmail.com>
2023-11-07 07:36:41 +0700

    fetching data

--- pages/home.js
+++ pages/home.js
@@ -1,12 +1,35 @@
-import Navbar from "./components/Navbar";
+import Head from 'next/head';
+import Navbar from './components/Navbar';
+import Image from 'next/image';
 
-const Home = () => {
-    return (
-        <>
-        <Navbar/>
-        <h1>Ini halaman Home</h1>
-        </>
-    );
+const home = ({ posts }) => {
+  return (
+    <>
+      <Head>
+        <title>home | Belajar Next JS</title>
+        <meta name="description" content="Belajar Next JS" />
+      </Head>
+      <Navbar />
+      <Image src="/next.svg" alt="Logo Next JS" width={100} height={100} />
+      <h1>Ini Halaman home</h1>
+      {posts.map((post) => (
+        <div key={post.id}>
+          <h2>{post.title}</h2>
+        </div>
+      ))}
+    </>
+  );
 };
 
-export default Home;
+export default home;
+
+export const getStaticProps = async () => {
+  const res = await fetch('https://jsonplaceholder.typicode.com/posts');
+  const posts = await res.json();
+
+  return {
+    props: {
+      posts,
+   },
+  };
+};
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,17 @@
-import Navbar from "./components/Navbar"
+import Image from 'next/image';
+import Navbar from './components/Navbar';
+import Head from 'next/head';
 
 export default function Home() {
   return (
     <>
-      <Navbar/>
-      <h1>Ini halaman Home</h1>
-    </>
-  )
+      <Head>
+        <title>Home | Belajar Next JS</title>
+        <meta name="description"  content="Belajar Next JS"/>
+      </Head>
+      <Navbar />
+      <Image src="/next.svg" alt="Logo Next JS" width={100} height={100} />
+      <h1>Ini Halaman Home</h1>
+    </>
+  );
 }
